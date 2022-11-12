7fca19fa5d078005030bc8000675639df1edf1f4
agus <agus.casarone@gmail.com>
2022-11-12 17:58:40 -0300

    spring

---
+++ odontologos/src/test/java/com/example/odontologos/OdontologosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.odontologos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontologosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
